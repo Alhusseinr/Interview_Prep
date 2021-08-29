8b351fcf3239925ed201020f89ff54ee8e25ed9a
Alhusseinr <Ramialhussein98@gmail.com>
2021-08-29 18:43:54 -0400

    deleting node

--- src/com/dataSctructures/ListNode.java
+++ src/com/dataSctructures/ListNode.java
@@ -4,6 +4,17 @@ public class ListNode {
     public int val;
     public ListNode next;
     public ListNode() { }
-    public ListNode(int val) { this.val = val; }
-    public ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+
+    public ListNode(int val) { 
+        this.val = val; 
+    }
+
+    public ListNode(int val, ListNode next) { 
+        this.val = val; this.next = next; 
+    }
+
+    public void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
 }
